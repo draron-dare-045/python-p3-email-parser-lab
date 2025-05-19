2716a13dbecfbf8af419da101aca077a73830e5b
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-05-19 13:27:45 +0300

    6 passed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        normalized = self.email_string.replace(",", " ")
+        parts = normalized.split()
+        email_pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
+        valid_emails = [email for email in parts if email_pattern.fullmatch(email)]
+        return sorted(set(valid_emails))
